4ae876bfbb9b81c18b37f1bfc3a2f3e90c7a351e
Kris <kris_s2@outlook.com>
2022-02-22 23:59:38 -0800

    Two new components

---
+++ src/components/car/Car.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Car(props) {
+
+    return(
+        <li>My {props.brand}</li>
+    );
+
+}
+
+export default Car;
+
---
+++ src/components/garage/Garage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Garage() {
+    return (
+        <div>
+            <h1>My Garage has:</h1>
+        </div>
+    );
+}
+
+export default Garage;
