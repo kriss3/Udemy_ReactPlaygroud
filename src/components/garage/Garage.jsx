e06a6312bf62ae49e6820a79abde9c9f0a482895
Kris <kris_s2@outlook.com>
2022-02-23 23:45:20 -0800

    Adding a usage of Car compnent inside Garage component - simple use of props;

--- src/components/garage/Garage.jsx
+++ src/components/garage/Garage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Car from './car';
 
 function Garage() {
+    const cars = ['Fiat','Renault','Opel'];
     return (
         <div>
             <h1>My Garage has:</h1>
+            {cars.map(c => <Car brand={c}/>)}
         </div>
     );
 }
