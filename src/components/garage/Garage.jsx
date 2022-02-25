8c609a8f1d1a209d1adb2f9529a8cf98dbc21d1f
Kris <kris_s2@outlook.com>
2022-02-24 23:45:24 -0800

    changing Grarage component to use arrow function; Remove hard coded data fetch inside the component;

--- src/components/garage/Garage.jsx
+++ src/components/garage/Garage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Car from '../car/Car';
 
-function Garage() {
+
+const getData = () => {
     const cars = ['Fiat','Renault','Opel'];
+    return cars;
+};
+
+const Garage = () => {
+    const cars = getData();
     return (
         <div>
             <h1>My Garage has:</h1>
