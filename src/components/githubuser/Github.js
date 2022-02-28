41c573fe68ce7f874fa020f20a2d0d100aed52c6
Kris <kris_s2@outlook.com>
2022-02-27 23:47:00 -0800

    Ranaming github component to GitHubUser; adding implementaion of useEffect

---
+++ src/components/githubuser/Github.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+
+
+function GitHubUser(props) {
+
+    const [data, setData] = useState(null);
+
+    const getData = () => {
+        let url = 'https://api.github.com/users/moonhighway';
+        fetch(url)
+        .then(res => res.json())
+        .then(setData)
+        .catch(console.error);
+    };
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    return (
+        <>
+            <h1>This component will display details of a github account.</h1>
+            <div>{JSON.stringify(data)}</div>  
+        </>
+    );
+}
+
+export default Github;
