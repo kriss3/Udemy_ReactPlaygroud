65b89a09a87c4e08bc5348d0995ad0d0746c17f3
Kris <kris_s2@outlook.com>
2022-02-27 23:37:04 -0800

    Creating new github component

---
+++ src/components/github/Github.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Github(props) {
+    return (
+        <div>
+            <h1>This component will display details of a github account.</h1>
+        </div>
+    );
+}
+
+export default Github;
